8aefc30c539ceb136c7623680dcfce68f2f8ecc5
ValentineLebedeva <lebedeva@phystech.edu>
2015-11-16 19:13:07 +0300

    mr

---
+++ Map.java
@@ -0,0 +1,30 @@
+package ru.jiht;
+
+import java.io.IOException;
+import java.util.*;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.w3c.dom.Text;
+
+public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+    private Text word = new Text();
+
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String line = value.toString();
+        StringTokenizer tokenizer = new StringTokenizer(line);
+        while (tokenizer.hasMoreTokens()) {
+            word.set(tokenizer.nextToken());
+            if (word.toString().charAt(0) = 'v') {
+                context.write(word, new IntWritable(1));
+            }
+        }
+    }
+}
+  
